5eeec4461d162cf0f6d1bdc425fe4c85e801f1e2
Marian <marianbabyti@gmail.com>
2023-03-31 08:59:39 -0300

    Ejercicio 1 finalizado

---
+++ src/main/kotlin/ejercicios/Ejercicio1.kt
@@ -0,0 +1,23 @@
+package ejercicios
+
+fun main (){
+
+    print("ingrese nonmbre de la calle : ")
+    var calle = readln()
+
+    print("ingrese nonmbre  de la ciudad : ")
+    var cuidad = readln()
+
+    print("ingrese nonmbre  del estado  : ")
+    var estado = readln()
+
+    print("ingrese el pais : ")
+    var pais = readln()
+
+    print("ingrese el codigo postal : ")
+    var codigoPostal = readln()
+
+     println("""\nDATOS""")
+
+    println(" $calle  , $cuidad , $estado ,$pais , $codigoPostal")
+}
